5fb0773ab7df14ab22c8c02f43787d91a90d26b4
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-11-11 23:39:36 -0500

    add img

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import img from "../img/globe-dark.png";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
+      <img src={img} alt="globe" />
       <p className="title-bio">Your vision, my execution</p>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -21,6 +24,11 @@ function App() {
 
 export default App;
 
-// dark balck graduation color
-// List with all the sercive we provide
+// style the img 
+// img {
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover;
+// }
 
+// List with all the sercive we provide
