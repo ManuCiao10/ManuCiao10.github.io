03684c156d9077b4b46f14f5056292428926185f
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-11-11 22:22:39 -0500

    Init agency

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <p className="title-bio">Your vision, my execution</p>
@@ -25,3 +23,4 @@ export default App;
 
 // dark balck graduation color
 // List with all the sercive we provide
+
