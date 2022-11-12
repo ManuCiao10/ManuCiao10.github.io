9e30a6adc9b63bd66ea6eff48e25e75130f5f400
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-11-12 11:11:45 -0500

    Color-Text

--- src/App.tsx
+++ src/App.tsx
@@ -6,47 +6,53 @@ import logo from "../img/favicon.png";
 
 function App() {
   return (
-    <div className="main-part">
-      <header className="header-full-size">
-        <div className="header-size-less">
-          <div className="header-nice-formatted">
-            <div className="header-left">
-              <a className="block-link" href="/">
-                <img className="logo" src={logo} alt="Logo" draggable="false" />
-              </a>
-            </div>
-            <div className="header-right">
-              <ul className="header-list">
-                <li className="header-discord">
-                  <a
-                    className="block-link"
-                    href=""
-                  ></a>
-                </li>
-                {/* <li className="header-github"> */}
-              </ul>
+    <>
+      <div className="main-part">
+        <header className="header-full-size">
+          <div className="header-size-less">
+            <div className="header-nice-formatted">
+              <div className="header-left">
+                <a className="block-link" href="/">
+                  <img
+                    className="logo"
+                    src={logo}
+                    alt="Logo"
+                    draggable="false"
+                  />
+                </a>
+              </div>
+              <div className="header-right">
+                <ul className="header-list">
+                  <li className="header-discord">
+                    <a className="block-link" href=""></a>
+                  </li>
+                  {/* <li className="header-github"> */}
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      </header>
-
+        </header>
+      </div>
       <div className="App">
         {/* <Header /> */}
 
         {/* <img src={img} alt="globe" className="globe" /> */}
-        <p className="title-bio">
-          Your vision <br></br>my execution
-        </p>
+        <div className="title-bio noHover">
+          Your vision<br></br>
+          <span className="title-bio-span noHover">my execution</span>
+        </div>
 
-        <p className="agency-intro">Your online presence to the next level.</p>
+        <p className="agency-intro noHover">Your online presence to the next level.</p>
       </div>
-    </div>
+    </>
   );
 }
 
 export default App;
 
 /*
+0. remove the text from mouseover
+0. [x] Fix Text alignment main phrase
 1. [+] Domain
 2. [+] Mare sfondo end of the page
 6. [+] Clean the code adding functions and components
