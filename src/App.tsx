6a751e6600f20b3ca1f92a940aecde115d2a5e30
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-11-12 00:22:20 -0500

    add img

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,13 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <img src={img} alt="globe" />
-      <p className="title-bio">Your vision, my execution</p>
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className="agency-intro">
-          web development agency. We provide experienced consultants, designers
-          and developers to take your online presence to the next level.
-        </p>
-      </div>
+      <img src={img} alt="globe" className="globe" />
+      <p className="title-bio">
+        Your vision <br></br>my execution
+      </p>
+      <p className="agency-intro">Your online presence to the next level.</p>
     </div>
   );
 }
 
 export default App;
-
-// style the img 
-// img {
-//   width: 100%;
-//   height: 100%;
-//   object-fit: cover;
-// }
-
-// List with all the sercive we provide
