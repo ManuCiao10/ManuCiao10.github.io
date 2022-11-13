6ded22d911b69c5412cdcc873fc9d3c7baa94f10
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-11-13 07:08:12 -0500

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ import { FaDiscord } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { AiFillLinkedin } from "react-icons/ai";
+import ReactTooltip from "react-tooltip";
 
 function App() {
   return (
-    <>
       <div className="main-part">
         <header className="header-full-size">
           <div className="header-size-less">
@@ -27,17 +27,86 @@ function App() {
                   />
                 </a>
               </div>
-              <div className="header-right">
-                <ul className="header-list">
-                  <li className="header-discord">
-                    <a className="block-link" href="f45g45"></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </header>
+              {/* Site navigation */}
+          <nav className="flex flex-grow">
+            <ul className="flex flex-grow justify-end flex-wrap items-center space-x-4">
+              <li className="hidden md:block">
+                <a
+                  data-tip
+                  data-for="discord"
+                  href="https://discord.gg/7PepvThQ9p"
+                >
+                  <FaDiscord
+                    className="fill-current text-black hover:text-blue-500 transition duration-300"
+                    size="1.5em"
+                  />
+                </a>
+              </li>
+              <li className="hidden md:block">
+                <a data-tip data-for="email" href="mailto:emanuele.ardinghi@gmail.com">
+                  <MdEmail
+                    className="fill-current text-black hover:text-blue-500 transition duration-300"
+                    size="1.5em"
+                  />
+                </a>
+              </li>
+              <li className="hidden md:block">
+                <a
+                  data-tip
+                  data-for="linkedin"
+                  href="https://www.linkedin.com/in/ian-hildebrand/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <ReactTooltip place="bottom" id="linkedin">
+                    {/* <span>Ian Hildebrand</span> */}
+                  </ReactTooltip>
+                  <AiFillLinkedin
+                    className="fill-current text-black hover:text-blue-500 transition duration-300"
+                    size="1.5em"
+                  />
+                </a>
+              </li>
+              <li className="hidden md:block">
+                <a
+                  data-tip
+                  data-for="github"
+                  href="https://github.com/ihildy"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub
+                    className="fill-current text-black hover:text-blue-500 transition duration-300"
+                    size="1.5em"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://discord.gg/7PepvThQ9p"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 group"
+                >
+                  <span>Contact</span>
+                  <svg
+                    className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 -mr-1 transition duration-300 transform ml-2 group-hover:translate-x-1"
+                    viewBox="0 0 12 12"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                      fillRule="nonzero"
+                    />
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
+    </header>
+        
       <div className="App">
         {/* <Header /> */}
 
@@ -59,14 +128,16 @@ function App() {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
+
 
 export default App;
 
 /*
 
+-add emoji to the header
 - fix responsive with iphone and mobile
 - add discord and check if is possible to cheate the button to pay with the world inside
 0. add bottom border to header
