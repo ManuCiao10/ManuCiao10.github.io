91e1f789cf2b8a84e117924743a547a7d847d432
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-11-13 12:32:48 -0500

    Contact button

--- src/App.tsx
+++ src/App.tsx
@@ -40,51 +40,14 @@ function App() {
                     />
                   </a>
                 </li> */}
-                <li className="hidden md:block">
+                <li>
                   <a
                     data-tip
                     data-for="email"
                     href="mailto:emanuele.ardinghi@gmail.com"
-                  >
-                    <MdEmail
-                      className="fill-current text-black hover:text-blue-500 transition duration-300"
-                      size="1.5em"
-                    />
-                  </a>
-                </li>
-                
-                {/* <li className="hidden md:block">
-                  <a
-                    data-tip
-                    data-for="github"
-                    href="https://github.com/ihildy"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <FaGithub
-                      className="fill-current text-black hover:text-blue-500 transition duration-300"
-                      size="1.5em"
-                    />
-                  </a>
-                </li> */}
-                <li>
-                  <a
-                    href="https://discord.gg/7PepvThQ9p"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 group"
+                    className="btn-sm "
                   >
                     <span>Contact</span>
-                    <svg
-                      className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 -mr-1 transition duration-300 transform ml-2 group-hover:translate-x-1"
-                      viewBox="0 0 12 12"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                        fillRule="nonzero"
-                      />
-                    </svg>
                   </a>
                 </li>
               </ul>
@@ -94,8 +57,6 @@ function App() {
       </header>
 
       <div className="App">
-        {/* <Header /> */}
-
         <div className="title-bio noHover noselect">
           Your vision<br></br>
           <span className="title-bio-span noHover noselect">my execution</span>
