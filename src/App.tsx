9678f7f2a785e3d217ebbdb7f04307bb7f520eb6
ManuCiao10 <emanuele.ardinghi@gmail.com>
2022-11-15 15:36:29 -0500

    Color - butto fixed

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import img from "../img/globe-dark.png";
 import Header from "./Header";
 import Footer from "./Footer";
-import background from "./background"
+import background from "./background";
 
 // new background();
 
@@ -16,18 +16,26 @@ function App() {
           Your vision<br></br>
           <span className="title-bio-span noHover noselect">my execution</span>
         </div>
-        <p className="agency-intro noHover noselect"></p>
+        {/* <p className="agency-intro noHover noselect"></p> */}
         <div className="grid-main noHover noselect">
           <a className="block-link">
-            <img src={img} alt="globe" className="globe" draggable="false" />
+            {/* <img src={img} alt="globe" className="globe" draggable="false" /> */}
           </a>
           <div className="grid-text">
-            <h1 className="grid-title noHover noselect"></h1>
+            {/* <h1 className="grid-title noHover noselect"></h1> */}
             <span className="grid-desc noHover noselect">
               Your online presence to the next level.
             </span>
-
-            <button className="button-get">Get in touch</button>
+            <li>
+              <a
+                data-tip
+                data-for="email"
+                href="mailto:emanuele.ardinghi@gmail.com"
+                className="btn-sm"
+              >
+                <span>Get in touch</span>
+              </a>
+            </li>
           </div>
         </div>
       </div>
